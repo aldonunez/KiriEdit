16384ba5ec8735a2a79e1d40bea4beecc5fde895
Aldo Nunez <aldonunez1@gmail.com>
2020-02-15 14:01:04 -0800

    Move OutlineRenderer to its own file.

--- TryFreetype/FigureWalker.cs
+++ TryFreetype/FigureWalker.cs
@@ -1,5 +1,4 @@
-﻿using SharpFont;
-using System;
+﻿using System;
 using System.Drawing;
 using TryFreetype.Model;
 using Point = TryFreetype.Model.Point;
@@ -29,7 +28,7 @@ namespace TryFreetype
                 {
                     var edge = point.OutgoingEdge;
 
-                    Pen pen = OnBeginEdge();
+                    OnBeginEdge();
 
                     switch (edge.Type)
                     {
@@ -76,9 +75,8 @@ namespace TryFreetype
         {
         }
 
-        protected virtual Pen OnBeginEdge()
+        protected virtual void OnBeginEdge()
         {
-            return null;
         }
 
         protected virtual void OnEndFigure()
@@ -96,15 +94,11 @@ namespace TryFreetype
         private Pen[] _pens;
         private int _nextPenIndex;
 
-        protected Figure figure { get; }
-
         public Bitmap Bitmap { get { return bitmap; } }
 
         public DebugFigureRenderer(Figure figure) :
             base(figure)
         {
-            this.figure = figure;
-
             int width = figure.Width;
             int height = figure.Height;
 
@@ -130,11 +124,11 @@ namespace TryFreetype
             _nextPenIndex = 0;
         }
 
-        protected override Pen OnBeginEdge()
+        protected override void OnBeginEdge()
         {
             int penIndex = _nextPenIndex;
             _nextPenIndex = (_nextPenIndex + 1) % _pens.Length;
-            return _pens[penIndex];
+            pen = _pens[penIndex];
         }
 
         protected override void OnEndFigure()
@@ -146,7 +140,7 @@ namespace TryFreetype
             Pen whitePen = new Pen(Color.White);
             int j = 0;
 
-            foreach (var group in figure.PointGroups)
+            foreach (var group in Figure.PointGroups)
             {
                 Point p = group.Points[0];
                 Pen pen = null;
@@ -242,339 +236,6 @@ namespace TryFreetype
                 });
             x = to.X;
             y = to.Y;
-        }
-    }
-
-    public class OutlineRenderer : FigureWalkerBase
-    {
-        private readonly Figure figure;
-        private readonly Bitmap bitmap;
-        private readonly Graphics g;
-
-        private double _x, _y;
-        private Pen pen;
-
-        public Bitmap Bitmap => bitmap;
-
-        public OutlineRenderer(Figure figure) :
-            base(figure)
-        {
-            this.figure = figure;
-
-            int width = figure.Width;
-            int height = figure.Height;
-
-            bitmap = new Bitmap(width, height);
-            Pen borderPen = new Pen(Color.White);
-
-            g = Graphics.FromImage(bitmap);
-            g.ScaleTransform(1, -1);
-            g.TranslateTransform(
-                (float) -figure.OffsetX,
-                -(height - 1) - (float) figure.OffsetY);
-
-            pen = new Pen(Color.Red);
-        }
-
-        public void Render()
-        {
-            foreach (var contour in figure.Contours)
-            {
-                MoveToFunc(contour.FirstPoint);
-
-                Point point = contour.FirstPoint;
-
-                for (int i = 0; true; i++)
-                {
-                    var edge = point.OutgoingEdge;
-
-                    switch (edge.Type)
-                    {
-                        case EdgeType.Line:
-                            LineToFunc(edge);
-                            break;
-
-                        case EdgeType.Conic:
-                            ConicToFunc(edge);
-                            break;
-
-                        case EdgeType.Cubic:
-                            CubicToFunc(edge);
-                            break;
-                    }
-
-                    point = edge.P2;
-
-                    if (point == contour.FirstPoint)
-                        break;
-                }
-            }
-        }
-
-        private int MoveToFunc(Point p)
-        {
-            var to = p;
-            Console.WriteLine("MoveTo: {0}, {1}", to.X, to.Y);
-            _x = to.X;
-            _y = to.Y;
-            return 0;
-        }
-
-        private int LineToFunc(Edge edge)
-        {
-            var to = edge.P2;
-            Console.WriteLine("LineTo: {0}, {1}", to.X, to.Y);
-            DrawLine(to);
-            _x = to.X;
-            _y = to.Y;
-            return 0;
-        }
-
-        private int ConicToFunc(Edge edge)
-        {
-            var control = ((ConicEdge) edge).Control1;
-            var to = edge.P2;
-            Console.WriteLine("ConicTo: {0},{1} {2},{3}", control.X, control.Y, to.X, to.Y);
-            DrawConic(control, to);
-            _x = to.X;
-            _y = to.Y;
-            return 0;
-        }
-
-        private int CubicToFunc(Edge edge)
-        {
-            var control1 = ((CubicEdge) edge).Control1;
-            var control2 = ((CubicEdge) edge).Control2;
-            var to = edge.P2;
-            Console.WriteLine("CubicTo: {0},{1} {2},{3} {4},{5}", control1.X, control1.Y, control2.X, control2.Y, to.X, to.Y);
-            DrawCubic(control1, control2, to);
-            _x = to.X;
-            _y = to.Y;
-            return 0;
-        }
-
-        private static double GetLength(Point p1, Point p2)
-        {
-            return p1.ToValuePoint().GetDistance(p2.ToValuePoint());
-        }
-
-        private static ValuePoint CalcConic(double t, Point p0, Point p1, Point p2)
-        {
-            var result = new ValuePoint();
-
-            result.X =
-                p0.X * Math.Pow((1 - t), 2) +
-                p1.X * 2 * t * (1 - t) +
-                p2.X * Math.Pow(t, 2)
-                ;
-
-            result.Y =
-                p0.Y * Math.Pow((1 - t), 2) +
-                p1.Y * 2 * t * (1 - t) +
-                p2.Y * Math.Pow(t, 2)
-                ;
-
-            return result;
-        }
-
-        private static ValuePoint CalcCubic(double t, Point p0, Point p1, Point p2, Point p3)
-        {
-            var result = new ValuePoint();
-
-            result.X =
-                p0.X * Math.Pow((1 - t), 3) +
-                p1.X * 3 * t * Math.Pow((1 - t), 2) +
-                p2.X * 3 * Math.Pow(t, 2) * (1 - t) +
-                p3.X * Math.Pow(t, 3)
-                ;
-
-            result.Y =
-                p0.Y * Math.Pow((1 - t), 3) +
-                p1.Y * 3 * t * Math.Pow((1 - t), 2) +
-                p2.Y * 3 * Math.Pow(t, 2) * (1 - t) +
-                p3.Y * Math.Pow(t, 3)
-                ;
-
-            return result;
-        }
-
-        private void DrawLine(Point to)
-        {
-            int x0 = RoundAndClampX(TransformX(_x));
-            int y0 = RoundAndClampY(TransformY(_y));
-            int x1 = RoundAndClampX(TransformX(to.X));
-            int y1 = RoundAndClampY(TransformY(to.Y));
-
-            DrawLine(x0, y0, x1, y1);
-        }
-
-        private void DrawLine(int x0, int y0, int x1, int y1)
-        {
-            // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-
-            int dx = Math.Abs(x1 - x0);
-            int sx = x0 < x1 ? 1 : -1;
-            int dy = -Math.Abs(y1 - y0);
-            int sy = y0 < y1 ? 1 : -1;
-
-            int err = dx + dy;
-
-            while (true)
-            {
-                bitmap.SetPixel(x0, y0, pen.Color);
-
-                if (x0 == x1 && y0 == y1)
-                    break;
-
-                int e2 = 2 * err;
-
-                if (e2 >= dy)
-                {
-                    err += dy;
-                    x0 += sx;
-                }
-                if (e2 <= dx)
-                {
-                    err += dx;
-                    y0 += sy;
-                }
-            }
-        }
-
-        private int RoundAndClampX(double x)
-        {
-            int iX = (int) Math.Round(x);
-            if (iX >= figure.Width)
-                iX = figure.Width - 1;
-            else if (iX < 0)
-                iX = 0;
-            return iX;
-        }
-
-        private int RoundAndClampY(double y)
-        {
-            int iY = (int) Math.Round(y);
-            if (iY >= figure.Height)
-                iY = figure.Height - 1;
-            else if (iY < 0)
-                iY = 0;
-            return iY;
-        }
-
-        private double TransformX(double x)
-        {
-            return x - figure.OffsetX;
-        }
-
-        private double TransformY(double y)
-        {
-            return figure.Height - y - 1 + figure.OffsetY;
-        }
-
-        private Point TransformPoint(Point point)
-        {
-            return new Point(
-                TransformX(point.X),
-                TransformY(point.Y));
-        }
-
-        private void DrawConic(Point control, Point to)
-        {
-            Point tFrom = new Point(
-                TransformX(_x),
-                TransformY(_y));
-            Point tControl = TransformPoint(control);
-            Point tTo = TransformPoint(to);
-
-            double length =
-                GetLength(tFrom, tControl)
-                + GetLength(tControl, tTo);
-
-            double dt = 1.0 / length;
-            ValuePoint p;
-            int x;
-            int y;
-            int prevX = RoundAndClampX(tFrom.X);
-            int prevY = RoundAndClampY(tFrom.Y);
-
-            for (double t = 0.0; t < 1.0; t += dt)
-            {
-                p = CalcConic(t, tFrom, tControl, tTo);
-
-                x = RoundAndClampX(p.X);
-                y = RoundAndClampY(p.Y);
-
-                if (((x - prevX) <= 1 && (x - prevX) >= -1)
-                    && ((y - prevY) <= 1 && (y - prevY) >= -1))
-                {
-                    bitmap.SetPixel(x, y, pen.Color);
-                }
-                else
-                {
-                    DrawLine(prevX, prevY, x, y);
-                }
-
-                prevX = x;
-                prevY = y;
-            }
-
-            p = CalcConic(1.0, tFrom, tControl, tTo);
-
-            x = RoundAndClampX(p.X);
-            y = RoundAndClampY(p.Y);
-
-            if (((x - prevX) <= 1 && (x - prevX) >= -1)
-                && ((y - prevY) <= 1 && (y - prevY) >= -1))
-            {
-                bitmap.SetPixel(x, y, pen.Color);
-            }
-            else
-            {
-                DrawLine(prevX, prevY, x, y);
-            }
-
-            bitmap.SetPixel(x, y, pen.Color);
-
-            // TODO: Find a way to get rid of bunches of pixels.
-            //       If the current (a) and last two pixels (b, c) fit in a 2x2 square,
-            //       then we can get rid of the previous one (b).
-        }
-
-        private void DrawCubic(Point control1, Point control2, Point to)
-        {
-            Point tFrom = new Point(
-                TransformX(_x),
-                TransformY(_y));
-            Point tControl1 = TransformPoint(control1);
-            Point tControl2 = TransformPoint(control2);
-            Point tTo = TransformPoint(to);
-
-            double length =
-                GetLength(tFrom, tControl1)
-                + GetLength(tControl1, tControl2)
-                + GetLength(tControl2, tTo);
-
-            double dt = 1.0 / length;
-            ValuePoint p;
-            int x;
-            int y;
-
-            for (double t = 0.0; t < 1.0; t += dt)
-            {
-                p = CalcCubic(t, tFrom, tControl1, tControl2, tTo);
-
-                x = RoundAndClampX(p.X);
-                y = RoundAndClampY(p.Y);
-
-                bitmap.SetPixel(x, y, pen.Color);
-            }
-
-            p = CalcCubic(1.0, tFrom, tControl1, tControl2, tTo);
-
-            x = RoundAndClampX(p.X);
-            y = RoundAndClampY(p.Y);
-
-            bitmap.SetPixel(x, y, pen.Color);
         }
     }
 }
---
+++ TryFreetype/OutlineRenderer.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Drawing;
+using TryFreetype.Model;
+using Point = TryFreetype.Model.Point;
+
+namespace TryFreetype
+{
+    public class OutlineRenderer : FigureWalkerBase
+    {
+        private readonly Bitmap _bitmap;
+
+        private double _x, _y;
+        private Color _color = Color.Red;
+
+        public Bitmap Bitmap => _bitmap;
+
+        public OutlineRenderer(Figure figure) :
+            base(figure)
+        {
+            int width = figure.Width;
+            int height = figure.Height;
+
+            _bitmap = new Bitmap(width, height);
+
+            // Transformation:
+            //g = Graphics.FromImage(bitmap);
+            //g.ScaleTransform(1, -1);
+            //g.TranslateTransform(
+            //    (float) -figure.OffsetX,
+            //    -(height - 1) - (float) figure.OffsetY);
+        }
+
+        protected override void OnMoveTo(Point p)
+        {
+            var to = p;
+            Console.WriteLine("MoveTo: {0}, {1}", to.X, to.Y);
+            _x = to.X;
+            _y = to.Y;
+        }
+
+        protected override void OnLineTo(Edge edge)
+        {
+            var to = edge.P2;
+            Console.WriteLine("LineTo: {0}, {1}", to.X, to.Y);
+            DrawLine(to);
+            _x = to.X;
+            _y = to.Y;
+        }
+
+        protected override void OnConicTo(Edge edge)
+        {
+            var control = ((ConicEdge) edge).Control1;
+            var to = edge.P2;
+            Console.WriteLine("ConicTo: {0},{1} {2},{3}", control.X, control.Y, to.X, to.Y);
+            DrawConic(control, to);
+            _x = to.X;
+            _y = to.Y;
+        }
+
+        protected override void OnCubicTo(Edge edge)
+        {
+            var control1 = ((CubicEdge) edge).Control1;
+            var control2 = ((CubicEdge) edge).Control2;
+            var to = edge.P2;
+            Console.WriteLine("CubicTo: {0},{1} {2},{3} {4},{5}", control1.X, control1.Y, control2.X, control2.Y, to.X, to.Y);
+            DrawCubic(control1, control2, to);
+            _x = to.X;
+            _y = to.Y;
+        }
+
+        private static double GetLength(Point p1, Point p2)
+        {
+            return p1.ToValuePoint().GetDistance(p2.ToValuePoint());
+        }
+
+        private static ValuePoint CalcConic(double t, Point p0, Point p1, Point p2)
+        {
+            var result = new ValuePoint();
+
+            result.X =
+                p0.X * Math.Pow((1 - t), 2) +
+                p1.X * 2 * t * (1 - t) +
+                p2.X * Math.Pow(t, 2)
+                ;
+
+            result.Y =
+                p0.Y * Math.Pow((1 - t), 2) +
+                p1.Y * 2 * t * (1 - t) +
+                p2.Y * Math.Pow(t, 2)
+                ;
+
+            return result;
+        }
+
+        private static ValuePoint CalcCubic(double t, Point p0, Point p1, Point p2, Point p3)
+        {
+            var result = new ValuePoint();
+
+            result.X =
+                p0.X * Math.Pow((1 - t), 3) +
+                p1.X * 3 * t * Math.Pow((1 - t), 2) +
+                p2.X * 3 * Math.Pow(t, 2) * (1 - t) +
+                p3.X * Math.Pow(t, 3)
+                ;
+
+            result.Y =
+                p0.Y * Math.Pow((1 - t), 3) +
+                p1.Y * 3 * t * Math.Pow((1 - t), 2) +
+                p2.Y * 3 * Math.Pow(t, 2) * (1 - t) +
+                p3.Y * Math.Pow(t, 3)
+                ;
+
+            return result;
+        }
+
+        private void DrawLine(Point to)
+        {
+            int x0 = RoundAndClampX(TransformX(_x));
+            int y0 = RoundAndClampY(TransformY(_y));
+            int x1 = RoundAndClampX(TransformX(to.X));
+            int y1 = RoundAndClampY(TransformY(to.Y));
+
+            DrawLine(x0, y0, x1, y1);
+        }
+
+        private void DrawLine(int x0, int y0, int x1, int y1)
+        {
+            // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+
+            int dx = Math.Abs(x1 - x0);
+            int sx = x0 < x1 ? 1 : -1;
+            int dy = -Math.Abs(y1 - y0);
+            int sy = y0 < y1 ? 1 : -1;
+
+            int err = dx + dy;
+
+            while (true)
+            {
+                _bitmap.SetPixel(x0, y0, _color);
+
+                if (x0 == x1 && y0 == y1)
+                    break;
+
+                int e2 = 2 * err;
+
+                if (e2 >= dy)
+                {
+                    err += dy;
+                    x0 += sx;
+                }
+                if (e2 <= dx)
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+        }
+
+        private int RoundAndClampX(double x)
+        {
+            int iX = (int) Math.Round(x);
+            if (iX >= Figure.Width)
+                iX = Figure.Width - 1;
+            else if (iX < 0)
+                iX = 0;
+            return iX;
+        }
+
+        private int RoundAndClampY(double y)
+        {
+            int iY = (int) Math.Round(y);
+            if (iY >= Figure.Height)
+                iY = Figure.Height - 1;
+            else if (iY < 0)
+                iY = 0;
+            return iY;
+        }
+
+        private double TransformX(double x)
+        {
+            return x - Figure.OffsetX;
+        }
+
+        private double TransformY(double y)
+        {
+            return Figure.Height - y - 1 + Figure.OffsetY;
+        }
+
+        private Point TransformPoint(Point point)
+        {
+            return new Point(
+                TransformX(point.X),
+                TransformY(point.Y));
+        }
+
+        private void DrawConic(Point control, Point to)
+        {
+            Point tFrom = new Point(
+                TransformX(_x),
+                TransformY(_y));
+            Point tControl = TransformPoint(control);
+            Point tTo = TransformPoint(to);
+
+            double length =
+                GetLength(tFrom, tControl)
+                + GetLength(tControl, tTo);
+
+            double dt = 1.0 / length;
+            ValuePoint p;
+            int x;
+            int y;
+            int prevX = RoundAndClampX(tFrom.X);
+            int prevY = RoundAndClampY(tFrom.Y);
+
+            for (double t = 0.0; t < 1.0; t += dt)
+            {
+                p = CalcConic(t, tFrom, tControl, tTo);
+
+                x = RoundAndClampX(p.X);
+                y = RoundAndClampY(p.Y);
+
+                if (((x - prevX) <= 1 && (x - prevX) >= -1)
+                    && ((y - prevY) <= 1 && (y - prevY) >= -1))
+                {
+                    _bitmap.SetPixel(x, y, _color);
+                }
+                else
+                {
+                    DrawLine(prevX, prevY, x, y);
+                }
+
+                prevX = x;
+                prevY = y;
+            }
+
+            p = CalcConic(1.0, tFrom, tControl, tTo);
+
+            x = RoundAndClampX(p.X);
+            y = RoundAndClampY(p.Y);
+
+            if (((x - prevX) <= 1 && (x - prevX) >= -1)
+                && ((y - prevY) <= 1 && (y - prevY) >= -1))
+            {
+                _bitmap.SetPixel(x, y, _color);
+            }
+            else
+            {
+                DrawLine(prevX, prevY, x, y);
+            }
+
+            _bitmap.SetPixel(x, y, _color);
+
+            // TODO: Find a way to get rid of bunches of pixels.
+            //       If the current (a) and last two pixels (b, c) fit in a 2x2 square,
+            //       then we can get rid of the previous one (b).
+        }
+
+        private void DrawCubic(Point control1, Point control2, Point to)
+        {
+            Point tFrom = new Point(
+                TransformX(_x),
+                TransformY(_y));
+            Point tControl1 = TransformPoint(control1);
+            Point tControl2 = TransformPoint(control2);
+            Point tTo = TransformPoint(to);
+
+            double length =
+                GetLength(tFrom, tControl1)
+                + GetLength(tControl1, tControl2)
+                + GetLength(tControl2, tTo);
+
+            double dt = 1.0 / length;
+            ValuePoint p;
+            int x;
+            int y;
+
+            for (double t = 0.0; t < 1.0; t += dt)
+            {
+                p = CalcCubic(t, tFrom, tControl1, tControl2, tTo);
+
+                x = RoundAndClampX(p.X);
+                y = RoundAndClampY(p.Y);
+
+                _bitmap.SetPixel(x, y, _color);
+            }
+
+            p = CalcCubic(1.0, tFrom, tControl1, tControl2, tTo);
+
+            x = RoundAndClampX(p.X);
+            y = RoundAndClampY(p.Y);
+
+            _bitmap.SetPixel(x, y, _color);
+        }
+    }
+}
