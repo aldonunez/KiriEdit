6542dc3314c001056c30cc3286680fa7786f5f01
Aldo Nunez <aldonunez1@gmail.com>
2020-03-02 16:25:59 -0800

    Add NewCharacterForm.

---
+++ KiriEdit/NewCharacterForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KiriEdit
+{
+    partial class NewCharacterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.charLabel = new System.Windows.Forms.Label();
+            this.charTextBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // charLabel
+            // 
+            this.charLabel.AutoSize = true;
+            this.charLabel.Location = new System.Drawing.Point(29, 33);
+            this.charLabel.Name = "charLabel";
+            this.charLabel.Size = new System.Drawing.Size(83, 20);
+            this.charLabel.TabIndex = 0;
+            this.charLabel.Text = "Character:";
+            // 
+            // charTextBox
+            // 
+            this.charTextBox.Location = new System.Drawing.Point(155, 33);
+            this.charTextBox.Name = "charTextBox";
+            this.charTextBox.Size = new System.Drawing.Size(100, 26);
+            this.charTextBox.TabIndex = 1;
+            this.charTextBox.TextChanged += new System.EventHandler(this.charTextBox_TextChanged);
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.okButton.Location = new System.Drawing.Point(93, 99);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(97, 37);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(196, 99);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(97, 37);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // NewCharacterForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(305, 148);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.charTextBox);
+            this.Controls.Add(this.charLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NewCharacterForm";
+            this.Text = "NewCharacterForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label charLabel;
+        private System.Windows.Forms.TextBox charTextBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ KiriEdit/NewCharacterForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KiriEdit
+{
+    public partial class NewCharacterForm : Form
+    {
+        public delegate bool ValidateCharHandler(uint codePoint);
+
+        public event ValidateCharHandler ValidateChar;
+
+        public uint CodePoint { get; private set; }
+
+        public NewCharacterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void charTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // TODO: make this more accurate for code points with more than 1 char.
+
+            okButton.Enabled = charTextBox.TextLength > 0;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            // TODO: make this more accurate for code points with more than 1 char.
+
+            uint codePoint = charTextBox.Text[0];
+
+            if (ValidateChar != null && !ValidateChar(CodePoint))
+                return;
+
+            CodePoint = codePoint;
+
+            DialogResult = DialogResult.OK;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
