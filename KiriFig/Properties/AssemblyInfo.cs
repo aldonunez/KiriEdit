4d3b3ec1d8be2ba2456006454da9692dc0ef6559
Aldo Nunez <aldonunez1@gmail.com>
2020-04-08 13:30:45 -0700

    Fix the assembly resource information of KiriFig.

--- KiriFig/Properties/AssemblyInfo.cs
+++ KiriFig/Properties/AssemblyInfo.cs
@@ -12,11 +12,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TryFreetype")]
+[assembly: AssemblyTitle("KiriFig")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TryFreetype")]
+[assembly: AssemblyProduct("KiriFig")]
 [assembly: AssemblyCopyright("Copyright ©  2020 Aldo J. Nunez")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
